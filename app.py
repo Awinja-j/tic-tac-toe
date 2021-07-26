9bd1d10b6778250cdeb07b1ff2592e19037a53f1
Awinja-j <ingari61@hotmail.com>
2021-07-26 23:47:26 +0300

    complete app

--- app.py
+++ app.py
@@ -1,214 +1,213 @@
 from flask import Flask, request, jsonify
 from werkzeug.exceptions import BadRequest
+import random
 
 app = Flask(__name__)
 
-class TicTacToe:
-    def __init__(self, board: list):
-        self.board =    [" ", " ", " ",
-                        " ", " ", " ",
-                        " ", " ", " "]
+
+board = ['.','.','.','.','.','.','.','.','.']
+
+human_player_board = list()
+
+continue_with_game = True # will be false if we have a tie or win
+
+winner = None # is none until winner is found, then 1
+
+current_player = None
+
+def display_board():
+    '''
+    This displays the board on the screen
+    '''
+    global board
+
+    print(board[0],board[1],board[2])
+    print(board[3],board[4],board[5])
+    print(board[6],board[7],board[8])
+
+def play_game(human_player_board):
+    '''
+    play game
+    '''
+    print(human_player_board, 'hhhhh')
+
+    # 1: if this game has not been won, then keep looping until wel get a winner!
+
+    while continue_with_game:
+
+            # human is in control of this game
+            human_player()
         
-        self.human_player_board = board
+    # when the game has been won, then we get out of the loop and print the winner
+    if winner == "x" or winner == "o":
+        return jsonify({"and the winner is": winner})
+    elif winner == None:
+        return jsonify({"its a tie": display_board()})
 
-        self.continue_with_game = True # will be false if we have a tie or win
-        
-        self.winner = None # is none until winner is found, then 1
+def computer_play():
+    print('computer is here there')
 
-        self.current_player = "o" # who is currently playing?
-
-    # def display_board(self):
-    #     '''
-    #     This displays the board on the screen
-    #     '''
-    #     print("\n")
-    #     print(self.board[0] + " | " + self.board[1] + " | " + self.board[2] + "     1 | 2 | 3")
-    #     print(self.board[3] + " | " + self.board[4] + " | " + self.board[5] + "     4 | 5 | 6")
-    #     print(self.board[6] + " | " + self.board[7] + " | " + self.board[8] + "     7 | 8 | 9")
-    #     print("\n")
-
-    # Play a game of tic tac toe
-    def play_game(self) -> dict:
-
-        # 1: if this game has not been won, then keep looping until wel get a winner!
-
-        while self.continue_with_game:
-
-            # 2: lets check who is currently playing, computer plays as o and human player is x
-            player = self.current_player()
-
-            # 3.if the current player is computer, then call self.computer_play()
-            if player == "o":
-                self.computer_play()
-            else: 
-                # 3a: human player passes  
-                self.human_player()
-        
-        # when the game has been won, then we get out of the loop and print the winner
-        if self.winner == "x" or self.winner == "o":
-            return jsonify({"and the winner is": self.winner})
-        elif self.winner == None:
-            return jsonify({"its a tie": ":-)"})
-
-    def computer_play(self):
-        # Handle a turn
-        self.computer_turn()
-
-        # Check if the game is over
-        self.check_if_game_over()
-
-        # Flip to the other player
-        self.flip_player()
-
-    def human_player(self):
-        # Handle a turn
-        self.human_turn()
-
-        # Check if the game is over
-        self.check_if_game_over()
-
-        # Flip to the other player
-        self.flip_player()
-
-    def computer_turn(self):
-        pass
-
-    # def human_turn(self):
-
-    #     # Get position from player
-    #     print(player + "'s turn.")
-    #     position = input("Choose a position from 1-9: ")
-
-    #     # Whatever the user inputs, make sure it is a valid input, and the spot is open
-    #     valid = False
-    #     while not valid:
-
-    #         # Make sure the input is valid
-    #         while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-    #             position = input("Choose a position from 1-9: ")
-        
-    #         # Get correct index in our board list
-    #         position = int(position) - 1
-
-    #         # Then also make sure the spot is available on the board
-    #         if self.board[position] == "-":
-    #             valid = True
-    #         else:
-    #             print("You can't go there. Go again.")
-
-    #     # Put the game piece on the board
-    #     self.board[position] = player
-
-    #     # Show the game board
-    #     self.display_board()
-
-    # def get_new_human_player_entry(self):
-    #     for key, value in enumerate(self.board):
-    #         for key
-    #     _
+    # Handle a turn
+    computer_turn()
 
     # Check if the game is over
-    def check_if_game_over(self):
-        self.check_for_winner()
-        self.check_for_tie()
+    check_if_game_over()
 
+    # Flip to the other player
+    flip_player()
 
-    # Check to see if somebody has won
-    def check_for_winner(self):
-        '''
-        Checks if there is a winner in rows, columns or in the two diagonals
-        '''
-        row_winner = self.check_rows()
-        column_winner = self.check_columns()
-        diagonal_winner = self.check_diagonals()
+    return jsonify({'your turn':display_board()})
 
-        if row_winner:
-            self.winner = row_winner
-        elif column_winner:
-            self.winner = column_winner
-        elif diagonal_winner:
-            self.winner = diagonal_winner
+def human_player():
+
+    # Handle a turn
+    human_turn()
+
+    # Check if the game is over
+    check_if_game_over()
+
+    # Flip to the other player
+    flip_player()
+
+    #after it is done playing, call computer to play
+    computer_play()
+
+def computer_turn():
+    global board
+    n = random.randint(0,9)
+    if board[n] == '.':
+        board[n] = 'o'
+
+# def human_turn():
+#     position = get_new_position()
+#     print('nnnn', position)
+
+#     if position == False:
+        
+
+#     # Put the game piece on the board
+#     board[int(position)] = 'x'
+
+def human_turn():
+
+    global board, human_player_board
+
+    for k,v in enumerate(human_player_board):
+        if board[k] == '.':
+            board[int(k)] = 'x'
         else:
-            self.winner = None
+            raise BadRequest('This position has been played')
 
 
-    def check_rows(self):
-        # Check if any of the rows have all the same value (and is not empty)
-        row_1 = self.board[0] == self.board[1] == self.board[2] != "-"
-        row_2 = self.board[3] == self.board[4] == self.board[5] != "-"
-        row_3 = self.board[6] == self.board[7] == self.board[8] != "-"
-        # If any row does have a match, flag that there is a win
-        if row_1 or row_2 or row_3:
-            self.continue_with_game = False
-        # Return the winner
-        if row_1:
-            return self.board[0] 
-        elif row_2:
-            return self.board[3] 
-        elif row_3:
-            return self.board[6] 
-        # Or return None if there was no winner
-        else:
-            return None
+# Check if the game is over
+def check_if_game_over():
+    check_for_winner()
+    check_for_tie()
 
 
-    # Check the columns for a win
-    def check_columns(self):
-        # Check if any of the columns have all the same value (and is not empty)
-        column_1 = self.board[0] == self.board[3] == self.board[6] != "-"
-        column_2 = self.board[1] == self.board[4] == self.board[7] != "-"
-        column_3 = self.board[2] == self.board[5] == self.board[8] != "-"
-        # If any row does have a match, flag that there is a win
-        if column_1 or column_2 or column_3:
-            self.continue_with_game = False
-        # Return the winner
-        if column_1:
-            return self.board[0] 
-        elif column_2:
-            return self.board[1] 
-        elif column_3:
-            return self.board[2] 
-        # Or return None if there was no winner
-        else:
-            return None
+# Check to see if somebody has won
+def check_for_winner():
+    '''
+    Checks if there is a winner in rows, columns or in the two diagonals
+    '''
+    row_winner = check_rows()
+    column_winner = check_columns()
+    diagonal_winner = check_diagonals()
+
+    if row_winner:
+        winner = row_winner
+    elif column_winner:
+        winner = column_winner
+    elif diagonal_winner:
+        winner = diagonal_winner
+    else:
+        winner = None
 
 
-    def check_diagonals(self):
-        # Check if any of the columns have all the same value (and is not empty)
-        diagonal_1 = self.board[0] == self.board[4] == self.board[8] != "-"
-        diagonal_2 = self.board[2] == self.board[4] == self.board[6] != "-"
-        # If any row does have a match, flag that there is a win
-        if diagonal_1 or diagonal_2:
-            self.continue_with_game = False
-        # Return the winner
-        if diagonal_1:
-            return self.board[0] 
-        elif diagonal_2:
-            return self.board[2]
-        # Or return None if there was no winner
-        else:
-            return None
+def check_rows():
+    global board
+    # Check if any of the rows have all the same value (and is not empty)
+    row_1 = board[0] == board[1] == board[2] != "."
+    row_2 = board[3] == board[4] == board[5] != "."
+    row_3 = board[6] == board[7] == board[8] != "."
+    # If any row does have a match, flag that there is a win
+    if row_1 or row_2 or row_3:
+        continue_with_game = False
+    # Return the winner
+    if row_1:
+        return board[0] 
+    elif row_2:
+        return board[3] 
+    elif row_3:
+        return board[6] 
+    # Or return None if there was no winner
+    else:
+        return None
+
+# Check the columns for a win
+def check_columns():
+    global board
+    # Check if any of the columns have all the same value (and is not empty)
+    column_1 = board[0] == board[3] == board[6] != "."
+    column_2 = board[1] == board[4] == board[7] != "."
+    column_3 = board[2] == board[5] == board[8] != "."
+    # If any row does have a match, flag that there is a win
+    if column_1 or column_2 or column_3:
+        continue_with_game = False
+    # Return the winner
+    if column_1:
+        return board[0] 
+    elif column_2:
+        return board[1] 
+    elif column_3:
+        return board[2] 
+    # Or return None if there was no winner
+    else:
+        return None
 
 
-    # Check if there is a tie
-    def check_for_tie(self):
-        # If board is full
-        if "-" not in self.board:
-            self.continue_with_game = False
-            return True
-        # Else there is no tie
-        else:
-            return False
+def check_diagonals():
+    global board
+    # Check if any of the columns have all the same value (and is not empty)
+    diagonal_1 = board[0] == board[4] == board[8] != "."
+    diagonal_2 = board[2] == board[4] == board[6] != "."
+    # If any row does have a match, flag that there is a win
+    if diagonal_1 or diagonal_2:
+        continue_with_game = False
+    # Return the winner
+    if diagonal_1:
+        return board[0] 
+    elif diagonal_2:
+        return board[2]
+    # Or return None if there was no winner
+    else:
+        return None
 
 
-    # Flip the current player from X to O, or O to X
-    def flip_player(self):
-        # If the current player was X, make it O
-        if self.current_player == "x":
-            self.current_player = "o"
-        # Or if the current player was O, make it X
-        elif self.current_player == "o":
-            self.current_player = "x"
+# Check if there is a tie
+def check_for_tie():
+    global board
+    # If board is full
+    if "." not in board:
+        continue_with_game = False
+        return True
+    # Else there is no tie
+    else:
+        return False
+
+
+# Flip the current player from X to O, or O to X depending on who has played
+def flip_player():
+
+    global current_player
+
+    # If the current player was X, make it O
+    if current_player == "x":
+        current_player = "o"
+    # Or if the current player was O, make it X
+    elif current_player == "o":
+        current_player = "x"
+
 
 @app.route("/", methods=['GET'])
 def index():
@@ -224,7 +223,6 @@ def index():
 
     board = list(board)
 
-    return TicTacToe.play_game(board)
+    game = play_game(board)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return game()
