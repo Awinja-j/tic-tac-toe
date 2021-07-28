b411bb1bfcf5f5c2c52ee1449ad33d46d90bb0df
Awinja-j <ingari61@hotmail.com>
2021-07-28 03:07:31 +0300

    final app

--- app.py
+++ app.py
@@ -4,210 +4,139 @@ import random
 
 app = Flask(__name__)
 
+#!/usr/bin/env python3
+from math import inf as infinity
+from random import choice
 
-board = ['.','.','.','.','.','.','.','.','.']
+HUMAN = -1
+COMP = +1
 
-human_player_board = list()
-
-continue_with_game = True # will be false if we have a tie or win
-
-winner = None # is none until winner is found, then 1
-
-current_player = None
-
-def display_board():
-    '''
-    This displays the board on the screen
-    '''
-    global board
-
-    print(board[0],board[1],board[2])
-    print(board[3],board[4],board[5])
-    print(board[6],board[7],board[8])
-
-def play_game(human_player_board):
-    '''
-    play game
-    '''
-    print(human_player_board, 'hhhhh')
-
-    # 1: if this game has not been won, then keep looping until wel get a winner!
-
-    while continue_with_game:
-
-            # human is in control of this game
-            human_player()
-        
-    # when the game has been won, then we get out of the loop and print the winner
-    if winner == "x" or winner == "o":
-        return jsonify({"and the winner is": winner})
-    elif winner == None:
-        return jsonify({"its a tie": display_board()})
-
-def computer_play():
-    print('computer is here there')
-
-    # Handle a turn
-    computer_turn()
-
-    # Check if the game is over
-    check_if_game_over()
-
-    # Flip to the other player
-    flip_player()
-
-    return jsonify({'your turn':display_board()})
-
-def human_player():
-
-    # Handle a turn
-    human_turn()
-
-    # Check if the game is over
-    check_if_game_over()
-
-    # Flip to the other player
-    flip_player()
-
-    #after it is done playing, call computer to play
-    computer_play()
-
-def computer_turn():
-    global board
-    n = random.randint(0,9)
-    if board[n] == '.':
-        board[n] = 'o'
-
-# def human_turn():
-#     position = get_new_position()
-#     print('nnnn', position)
-
-#     if position == False:
-        
-
-#     # Put the game piece on the board
-#     board[int(position)] = 'x'
-
-def human_turn():
-
-    global board, human_player_board
-
-    for k,v in enumerate(human_player_board):
-        if board[k] == '.':
-            board[int(k)] = 'x'
-        else:
-            raise BadRequest('This position has been played')
-
-
-# Check if the game is over
-def check_if_game_over():
-    check_for_winner()
-    check_for_tie()
-
-
-# Check to see if somebody has won
-def check_for_winner():
-    '''
-    Checks if there is a winner in rows, columns or in the two diagonals
-    '''
-    row_winner = check_rows()
-    column_winner = check_columns()
-    diagonal_winner = check_diagonals()
-
-    if row_winner:
-        winner = row_winner
-    elif column_winner:
-        winner = column_winner
-    elif diagonal_winner:
-        winner = diagonal_winner
+def evaluate(state):
+    
+    if wins(state, COMP):
+        score = +1
+    elif wins(state, HUMAN):
+        score = -1
     else:
-        winner = None
+        score = 0
+
+    return score
 
 
-def check_rows():
-    global board
-    # Check if any of the rows have all the same value (and is not empty)
-    row_1 = board[0] == board[1] == board[2] != "."
-    row_2 = board[3] == board[4] == board[5] != "."
-    row_3 = board[6] == board[7] == board[8] != "."
-    # If any row does have a match, flag that there is a win
-    if row_1 or row_2 or row_3:
-        continue_with_game = False
-    # Return the winner
-    if row_1:
-        return board[0] 
-    elif row_2:
-        return board[3] 
-    elif row_3:
-        return board[6] 
-    # Or return None if there was no winner
-    else:
-        return None
-
-# Check the columns for a win
-def check_columns():
-    global board
-    # Check if any of the columns have all the same value (and is not empty)
-    column_1 = board[0] == board[3] == board[6] != "."
-    column_2 = board[1] == board[4] == board[7] != "."
-    column_3 = board[2] == board[5] == board[8] != "."
-    # If any row does have a match, flag that there is a win
-    if column_1 or column_2 or column_3:
-        continue_with_game = False
-    # Return the winner
-    if column_1:
-        return board[0] 
-    elif column_2:
-        return board[1] 
-    elif column_3:
-        return board[2] 
-    # Or return None if there was no winner
-    else:
-        return None
-
-
-def check_diagonals():
-    global board
-    # Check if any of the columns have all the same value (and is not empty)
-    diagonal_1 = board[0] == board[4] == board[8] != "."
-    diagonal_2 = board[2] == board[4] == board[6] != "."
-    # If any row does have a match, flag that there is a win
-    if diagonal_1 or diagonal_2:
-        continue_with_game = False
-    # Return the winner
-    if diagonal_1:
-        return board[0] 
-    elif diagonal_2:
-        return board[2]
-    # Or return None if there was no winner
-    else:
-        return None
-
-
-# Check if there is a tie
-def check_for_tie():
-    global board
-    # If board is full
-    if "." not in board:
-        continue_with_game = False
+def wins(state, player):
+    
+    win_state = [
+        [state[0][0], state[0][1], state[0][2]],
+        [state[1][0], state[1][1], state[1][2]],
+        [state[2][0], state[2][1], state[2][2]],
+        [state[0][0], state[1][0], state[2][0]],
+        [state[0][1], state[1][1], state[2][1]],
+        [state[0][2], state[1][2], state[2][2]],
+        [state[0][0], state[1][1], state[2][2]],
+        [state[2][0], state[1][1], state[0][2]],
+    ]
+    if [player, player, player] in win_state:
         return True
-    # Else there is no tie
     else:
         return False
 
 
-# Flip the current player from X to O, or O to X depending on who has played
-def flip_player():
+def game_over(state):
+    
+    return wins(state, HUMAN) or wins(state, COMP)
 
-    global current_player
 
-    # If the current player was X, make it O
-    if current_player == "x":
-        current_player = "o"
-    # Or if the current player was O, make it X
-    elif current_player == "o":
-        current_player = "x"
+def empty_cells(state):
+    
+    cells = []
 
+    for x, row in enumerate(state):
+        for y, cell in enumerate(row):
+            if cell == 0:
+                cells.append([x, y])
+
+    return cells
+
+
+def valid_move(x, y, game_board):
+    
+    board = game_board
+    if [x, y] in empty_cells(board):
+        return True
+    else:
+        return False
+
+
+def set_move(x, y, player, game_board):
+    
+    board = game_board
+    if valid_move(x, y, board):
+        board[x][y] = player
+        return True
+    else:
+        return False
+
+
+def minimax(state, depth, player):
+    
+    if player == COMP:
+        best = [-1, -1, -infinity]
+    else:
+        best = [-1, -1, +infinity]
+
+    if depth == 0 or game_over(state):
+        score = evaluate(state)
+        return [-1, -1, score]
+
+
+    for cell in empty_cells(state):
+        x, y = cell[0], cell[1]
+        state[x][y] = player
+        score = minimax(state, depth - 1, -player)
+        state[x][y] = 0
+        score[0], score[1] = x, y
+
+        if player == COMP:
+            if score[2] > best[2]:
+                best = score  # max value
+        else:
+            if score[2] < best[2]:
+                best = score  # min value
+
+    return best
+
+def computer_turn(game_board: list) -> list:
+    print('i am listening')
+    board = game_board
+    depth = len(empty_cells(board))  #number of empty cells
+
+    if depth == 0 or game_over(board): #if there isnt any, end game
+        return
+
+    if depth == 9: # if the board has not been played...
+        x = choice([0, 1, 2]) # randomly choose from the three ints and set
+        y = choice([0, 1, 2]) # randomly choose from the three ints and set
+    else:
+        move = minimax(board, depth, COMP)
+        x, y = move[0], move[1]
+
+    h = set_move(x, y, COMP, game_board) # to the gameboard, set the choices
+
+    if h == True:
+        return game_board
+
+def main(game_board: list) -> list:
+    """
+    We assume that the human has played when a board is passed
+    when the board is passed as [[0,0,0],[0,0,0],[0,0,0]], this means human has 
+    not played and computer needs to play first.
+    """
+    print('i am here')
+
+    if len(empty_cells(game_board)) == 0: # if the board does not have any empty spaces, just return the board as is
+        return game_board
+
+    computer_turn(game_board) # otherwise computer will play
 
 @app.route("/", methods=['GET'])
 def index():
@@ -223,6 +152,46 @@ def index():
 
     board = list(board)
 
-    game = play_game(board)
+    board = list(divide_to_chunks(board, 3))
 
-    return game()
+    print('nnnnnnn', board)
+
+    game = main(board)
+
+    print('ddddddd', game)
+
+    game_results = change_to_string(game)
+
+    return game_results
+
+def change_to_nums(list_items):
+    for k,v in enumerate(list_items):
+        if v == 'x':
+            list_items[k] = -1
+        elif v == 'o':
+            list_items[k] = +1
+        elif v.isspace() == True:
+            list_items[k] = 0
+    return list_items
+
+def divide_to_chunks(j,n):
+    l = change_to_nums(j)
+    for i in range(0, len(l), n): 
+        yield l[i:i + n]
+
+def change_back(x):
+      for k,v in enumerate(x): 
+        if v == 1:
+            x[k] = 'o'
+        elif v == -1:
+            x[k] = 'x'
+        elif v == 0:
+            x[k] = ''
+        return x
+
+def change_to_string(t):
+    flat_list = [item for sublist in t for item in sublist]
+    n = change_back(flat_list)
+    m = ' '.join([str(elem) for elem in n])
+
+    return m
